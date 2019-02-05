22cd63877d23ee77823fd1102965f7a003b58588
Raúl Ojeda <rauloojs@gmail.com>
2019-02-04 19:54:34 -0600

    feature(App): Load bootstrap styles. Add Router. Set LoginPage as index route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Layout from './components/Layout';
+import LoginPage from './containers/LoginPage';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Layout>
+          <Router>
+            <Route path='/' exact component={LoginPage} />
+          </Router>
+        </Layout>
       </div>
     );
   }
